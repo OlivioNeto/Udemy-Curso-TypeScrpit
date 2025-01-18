b174ed2a15fc46b1bba1065b080e373f1702b78e
Olivio neto <netoolivio34@gmail.com>
2025-01-18 16:36:10 -0300

    Primeiro módulo do curso concluido

---
+++ 1_INTRO/index.js
@@ -0,0 +1,9 @@
+var numberOne = 10;
+var numberTwo = 5;
+function greeting(num1, num2) {
+    console.log("A soma é " + (num1 + num2));
+}
+greeting(numberOne, numberTwo);
+// greeting(numberTwo)
+// greeting(anotherName)
+// greeting(x)
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,12 @@
+const numberOne = 10
+const numberTwo = 5
+
+function greeting(num1: number, num2: number){
+    console.log("A soma é " + (num1 + num2))
+}
+
+greeting(numberOne, numberTwo)
+// greeting(numberTwo)
+// greeting(anotherName)
+// greeting(x)
+
