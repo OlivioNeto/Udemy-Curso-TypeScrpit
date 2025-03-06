bfa39f000081fb2c75d6c13fad17b0f72e6e4dc2
Olivio neto <netoolivio34@gmail.com>
2025-03-06 15:36:04 -0300

    Iniciando o terceiro módulo. Aprendendo array

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - Arrays
+let numbers = [1, 2, 3];
+numbers.push(3);
+console.log(numbers[2]);
+// numbers = "teste"
+const nomes = ["Olívio", "Nicolly"];
+// nomes.push(4)
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,12 @@
+// 1 - Arrays
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(3)
+
+console.log(numbers [2])
+
+// numbers = "teste"
+
+const nomes: string[] = ["Olívio", "Nicolly"]
+
+// nomes.push(4)
